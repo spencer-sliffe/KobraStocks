fce268a822bef7714ee279cf55b79a5849184d00
Spencer Sliffe <spenceronw@ku.edu>
2024-10-29 11:03:36 -0500

    Navbar Clean Up state wise

--- client/src/components/AppNavbar.vue
+++ client/src/components/AppNavbar.vue
@@ -1,26 +1,28 @@
 <template>
-  <nav>
+  <nav v-if="authState.isAuthenticated">
     <h1>KobraStocks</h1>
     <div class="hamburger" @click="toggleMenu">☰</div>
     <ul :class="{ show: menuVisible }">
-      <li><router-link to="/home">Home</router-link></li>
+      <li><router-link to="/homepage">Home</router-link></li>
       <li><router-link to="/about">About</router-link></li>
       <li><router-link to="/services">Services</router-link></li>
       <li><router-link to="/contact">Contact</router-link></li>
-      <li v-if="authState.isAuthenticated">
-        <router-link to="/account">Account</router-link>
-      </li>
-      <li v-if="authState.isAuthenticated">
-        <button @click="logout">Logout</button>
-      </li>
-      <li v-else>
-        <router-link to="/">Login</router-link>
-      </li>
+      <li><router-link to="/account">Account</router-link></li>
+      <li><button @click="logout">Logout</button></li>
+
       <li v-if="authState.isAuthenticated">
       </li>
       <li v-else>
-        <router-link to="/signup">Sign Up</router-link>
+
       </li>
+    </ul>
+  </nav>
+  <nav v-else>
+    <h1>KobraStocks</h1>
+    <div class="hamburger" @click="toggleMenu">☰</div>
+    <ul :class="{ show: menuVisible }">
+      <li ><router-link to="/signup">Sign Up</router-link></li>
+      <li ><router-link to="/">Login</router-link></li>
     </ul>
   </nav>
 </template>
