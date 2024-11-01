3aefbe0abb9e4cec76941df64e915f26b44dcaa8
Spencer Sliffe <spenceronw@ku.edu>
2024-11-01 09:31:54 -0500

    T-33 Advanced Search
    Proof of Concept

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,36 +1,6 @@
 <template>
   <div>
     <SearchBar :indicators="indicators"/>
-    <div class="cards-container">
-      <label class="card" :class="{ active: indicators.RSI }">
-        <input type="checkbox" v-model="indicators.RSI" hidden/>
-        <h2>RSI (Relative Strength Index)</h2>
-        <p>A momentum oscillator that measures the speed and change of price movements.</p>
-        <p>RSI is typically used to identify overbought or oversold conditions in a market.</p>
-      </label>
-      <label class="card" :class="{ active: indicators.MACD }">
-        <input type="checkbox" v-model="indicators.MACD" hidden/>
-        <h2>MACD (Moving Average Convergence Divergence)</h2>
-        <p>A trend-following momentum indicator that shows the relationship between two moving averages of a security’s
-          price.</p>
-        <p>MACD is used to spot changes in the strength, direction, momentum, and duration of a trend in a stock's
-          price.</p>
-      </label>
-      <label class="card" :class="{ active: indicators.MA50 }">
-        <input type="checkbox" v-model="indicators.MA50" hidden/>
-        <h2>MA50 (50-Day Moving Average)</h2>
-        <p>An indicator that averages out the closing prices for the last 50 days and updates with each new closing
-          price.</p>
-        <p>It's used to analyze the mid-term trend and determine support or resistance levels.</p>
-      </label>
-
-      <label class="card" :class="{ active: indicators.MA9 }">
-        <input type="checkbox" v-model="indicators.MA9" hidden/>
-        <h2>MA9 (9-Day Moving Average)</h2>
-        <p>A short-term moving average that averages the closing prices over the past 9 days.</p>
-        <p>Often used by traders to spot short-term trend reversals and as a component of the MACD.</p>
-      </label>
-    </div>
     <section class="favorite-stocks">
       <h2>Your Favorite Stocks</h2>
       <carousel :items-to-show="3" :wrap-around="true">
