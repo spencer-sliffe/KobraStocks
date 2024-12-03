037c23db58523afab987499accdaa5459b1288fe
Spencer Sliffe <spenceronw@ku.edu>
2024-12-03 17:48:39 -0600

    Updated backend url

--- client/src/main.js
+++ client/src/main.js
@@ -12,7 +12,7 @@ import router from './router';
 import './assets/css/style.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000';
+axios.defaults.baseURL = 'https://kobrastocks-backend-bse7h2dnhvhjefcd.canadacentral-01.azurewebsites.net';
 
 const token = localStorage.getItem('token');
 if (token) {
